b43213c6a4b29ebe460524b05a73a34549e0f0db
Tomohiro Murota <tomohiro.murota@gmail.com>
2015-12-14 00:36:48 +0900

    manage dotfiles as symlink

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# change dir temporarily
+CURRENT_DIR=$(pwd)
+DOTFILES_DIR=$(dirname $0)
+cd $DOTFILES_DIR
+DOTFILES_DIR=$(pwd)
+
+for ITEM in .??*
+do
+   # ignore pattern
+   [[ "$ITEM" == *'.git' ]] && continue
+   [[ "$ITEM" == *'.DS_Store' ]] && continue
+   ln -snfv "$DOTFILES_DIR/$ITEM" "$HOME/$ITEM"
+done
+
+# revert current dir
+cd $CURRENT_DIR
+
